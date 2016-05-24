f317a1fcdb7c9cdec23005dd8c937239ddd59774
MerelvanEmpel <merelvanempel@gmail.com>
2016-05-24 11:11:14 +0200

    Fix Webhook

--- src/softwareComponents/DDGameAPIComponent.php
+++ src/softwareComponents/DDGameAPIComponent.php
@@ -51,7 +51,7 @@ class DDGameAPIComponent {
 			$agent = CrowdAgent::where('_id', "crowdagent/biocrowd/" . $entity['user_id'])->first();
 			if($agent) {
 				// do not delete this on rollback
-				if(!array_key_exists($agent->_id, $this->crowdAgents)) {
+				if(!array_key_exists($agent->_id, $collection)) {
 					$agent->_existing = true;			
 				}
 			} else {
